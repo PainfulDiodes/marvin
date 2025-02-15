d85a343b2989c5616aa1e6634bd9fc67b85e5351
Stephen Willcock <160266765+PainfulDiodes@users.noreply.github.com>
2025-02-15 11:08:00 +0000

    Update build scripts

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
-sjasmplus --raw=bin --lst=lst *.asm
-hexdump -C bin > hex
+# usage: ./build.sh myfile.asm
+set -x      #echo on
+f=${1%.*}   #extract base filename
+sjasmplus --lst=$f.lst --lstlab --raw=$f.bin --dirbol $1
+hexdump -C $f.bin > $f.hex
