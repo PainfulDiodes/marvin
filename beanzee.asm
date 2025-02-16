c7de4172ccaf9c5bd183da473a4e1444ab2e5787
Stephen Willcock <160266765+PainfulDiodes@users.noreply.github.com>
2025-02-16 08:28:38 +0000

    Update beanzee.asm
    tidy

--- beanzee.asm
+++ beanzee.asm
@@ -1,7 +1,11 @@
-include stdmacro.asm
+; MARVIN build for beanzee / Z80 breadboard computer
+; https://github.com/PainfulDiodes/BeanZee/tree/v1.0.0
+; https://github.com/PainfulDiodes/z80-breadboard-computer/tree/v1.0.0
+
+; include stdmacro.asm
 
 BUFFER .equ 0x8000      ; input buffer - start of RAM
-STACK .equ 0xffff       ; this should really be 0x0000 as the CPU will dec SP before PUSH
+STACK  .equ 0xffff      ; this should really be 0x0000 as the CPU will dec SP before PUSH
 
     ld sp, STACK
     jp start
