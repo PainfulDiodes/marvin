500cfc7a394f1b8d413a1818a1d98550281956ae
Stephen Willcock <stephen.willcock@gmail.com>
2025-02-26 07:12:21 +0000

    Shell script updates

--- pack.sh
+++ pack.sh
@@ -1,3 +1,4 @@
-# usage: ./pack.sh test1.bin 0x9000 
+# usage: ./pack.sh test1 0x9000 
+#        ./pack.sh test1.bin 0x9000 
 f=${1%.*}   #extract base filename
 z88dk-appmake +hex --org $2 -b $f.bin -o $f.ihx
