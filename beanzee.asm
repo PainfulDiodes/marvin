52fd37f3db2b7fa1fee2d7ab709f45af6dfaa769
Stephen Willcock <stephen.willcock@gmail.com>
2025-02-28 08:04:31 +0000

    syntax changes for z88dk-z80asm
    Single quotes replace double quotes on chars.
    
    Labels must have preceding . or trailing :
    
    remove . from .equ
    
    Include filename needs double quotes

--- beanzee.asm
+++ beanzee.asm
@@ -2,14 +2,14 @@
 ; https://github.com/PainfulDiodes/BeanZee/tree/v1.0.0
 ; https://github.com/PainfulDiodes/z80-breadboard-computer/tree/v1.0.0
 
-BUFFER .equ 0xf000          ; input buffer - start of system RAM
-STACK  .equ 0xffff          ; this should really be 0x0000 as the CPU will dec SP before PUSH
+BUFFER equ 0xf000          ; input buffer - start of system RAM
+STACK  equ 0xffff          ; this should really be 0x0000 as the CPU will dec SP before PUSH
 
     ld sp, STACK
     jp start
 
-include UM245R.asm
-UM245R_CTRL .equ 0          ; serial control port
-UM245R_DATA .equ 1          ; serial data port
+include "UM245R.asm"
+UM245R_CTRL equ 0          ; serial control port
+UM245R_DATA equ 1          ; serial data port
 
-include marvin.asm
+include "marvin.asm"
