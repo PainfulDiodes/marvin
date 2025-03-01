da0ff63963289d6cfc9eb914d82ecdcdbdbffa19
Stephen Willcock <160266765+PainfulDiodes@users.noreply.github.com>
2025-03-01 08:16:39 +0000

    separate builds for different assemblers

---
+++ build-z88dk.sh
@@ -0,0 +1,16 @@
+# usage: 
+# with or without extension
+#  ./build-z88dk.sh beanzee
+#  ./build-z88dk.sh beanzee.asm
+# provide an org value to pack a HEX file
+#  ./build-z88dk.sh beanzee 0x8000
+
+# set -x      #echo on
+
+f=${1%.*}   #extract base filename
+z88dk-z80asm -l -b $f.asm
+hexdump -C $f.bin > $f.hdp
+if [ $# -gt 1 ]
+then
+    z88dk-appmake +hex --org $2 -b $f.bin -o $f.hex
+fi
--- build.sh
+++
@@ -1,6 +0,0 @@
-# usage: ./build.sh beanzee
-#        ./build.sh beanzee.asm
-set -x      #echo on
-f=${1%.*}   #extract base filename
-sjasmplus --lst=$f.lst --lstlab --raw=$f.bin --dirbol $f.asm
-hexdump -C $f.bin > $f.hdp
