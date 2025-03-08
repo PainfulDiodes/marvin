e1d9b2518ab320e45e74c4eb92652f401c136b8a
Stephen Willcock <160266765+PainfulDiodes@users.noreply.github.com>
2025-03-08 07:10:11 +0000

    Make z88dk the default build.sh
    Update README.md
    and rename shell script

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 # usage: 
 # with or without extension
-#  ./build-z88dk.sh beanzee
-#  ./build-z88dk.sh beanzee.asm
+#  ./build.sh beanzee
+#  ./build.sh beanzee.asm
 # provide an org value to pack an Intel HEX file
-#  ./build-z88dk.sh beanzee 0x8000
+#  ./build.sh beanzee 0x8000
 
 set -x #echo on
 
