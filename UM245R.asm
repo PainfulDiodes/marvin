9b4caf48cf47637565b7aad5e7e45f8a96750f09
Stephen Willcock <160266765+PainfulDiodes@users.noreply.github.com>
2025-03-08 10:19:24 +0000

    Update UM245R.asm
    CRLF transformations per VT100 at the USB level

--- UM245R.asm
+++ UM245R.asm
@@ -4,20 +4,33 @@
 ; As per:
 ; https://github.com/PainfulDiodes/z80-breadboard-computer/tree/v1.0.0
 
-getchar:                    ; get character from the UM245E and return in A
+getchar:                    ; get character and return in A
     in a,(UM245R_CTRL)      ; get the USB status
     bit 1,a                 ; data to read? (active low)
     jr nz,getchar           ; no, the buffer is empty
     in a,(UM245R_DATA)      ; yes, read the received char
+    cp _r                   ; is CR?
+    ret nz                  ; no - return
+    ld a, _n                ; convert CR to LF
     ret 
 
-putchar:                    ; transmit character in A via the UM245E 
+putchar:
+    cp _n                   ; newline?
+    jr nz, doputc           ; no - just send the char
+    ld a, _r
+    call putc
+    ld a, _n
+doputc:
+    call putc
+    ret
+
+putc:                       ; transmit character in A
     push bc
     ld b,a                  ; save the transmit character
-putcharloop: 
+putcloop: 
     in a,(UM245R_CTRL)      ; get the USB status
     bit 0,a                 ; ready to transmit? (active low)
-    jr nz,putcharloop       ; no, bit is high
+    jr nz,putcloop          ; no, bit is high
     ld a,b                  ; yes, restore the transmit character
     out  (UM245R_DATA),a    ; transmit the character
     pop bc
@@ -29,7 +42,7 @@ puts_loop:
     ld a,(hl)               ; get character from string
     cp 0                    ; is it zero?
     jr z, puts_end          ; yes - return
-    out(UM245R_DATA),a      ; no - send character
+    call putchar            ; no - send character
     inc hl                  ; next character position
     jp puts_loop            ; loop for next character
 puts_end:
