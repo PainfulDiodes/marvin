e8771e68d504daf76dd18e058e804eea9d79744d
Stephen Willcock <160266765+PainfulDiodes@users.noreply.github.com>
2025-04-22 07:23:21 +0100

    Create beanzee-ram.asm

---
+++ beanzee-ram.asm
@@ -0,0 +1,18 @@
+; MARVIN build for beanzee / Z80 breadboard computer
+; https://github.com/PainfulDiodes/BeanZee/tree/v1.0.0
+; https://github.com/PainfulDiodes/z80-breadboard-computer/tree/v1.0.0
+
+ORG 0x8000
+
+RAMSTART equ 0x9000        ; start of user RAM
+BUFFER   equ 0xf000        ; input buffer - start of system RAM
+STACK    equ 0xffff        ; this should really be 0x0000 as the CPU will dec SP before PUSH
+
+    ld sp, STACK
+    jp start
+
+include "UM245R.asm"
+UM245R_CTRL equ 0          ; serial control port
+UM245R_DATA equ 1          ; serial data port
+
+include "marvin.asm"
