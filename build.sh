365474fd95a4d3f9098c836b0a6551db25baf3c0
Stephen Willcock <stephen.willcock@gmail.com>
2025-05-10 09:53:45 +0100

    ORGDEF in build scripts for both assemblers

--- build.sh
+++ build.sh
@@ -5,12 +5,12 @@
 # provide an org value (defaults to 0x0000)
 #  ./build.sh beanzee $8000
 
-# set -x #echo on
+# set -x #echo on
 
 f=${1%.*} #extract base filename
 if [ $# -gt 1 ]
 then
-    z88dk-z80asm -l -b -m -r$2 $f.asm
+    z88dk-z80asm -l -b -m -DORGDEF=$2 $f.asm
     hexdump -C $f.bin > $f.hex
     z88dk-appmake +hex --org $2 -b $f.bin -o $f.ihx
 else
