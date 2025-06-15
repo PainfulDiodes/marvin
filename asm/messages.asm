14dc3da4b11bc30de01ae7971a1e717e8acaf3aa
Stephen Willcock <160266765+PainfulDiodes@users.noreply.github.com>
2025-06-15 07:55:34 +0100

    messages: v1.1.1_beta

--- asm/messages.asm
+++ asm/messages.asm
@@ -1,11 +1,17 @@
+ALIGN 0x10
+
 welcome_msg:
-                db "MARVIN v1.1.0\n"
+                db "MARVIN v1.1.1_beta\n"
                 db "A simple Z80 homebrew monitor program\n"
                 db "(c) Stephen Willcock 2024\n"
                 db "https://github.com/PainfulDiodes\n\n",0
 
+ALIGN 0x10
+
 bad_cmd_msg:    
                 db "Command not recognised\n",0
+
+ALIGN 0x10
 
 cmd_w_null_msg: 
                 db "No data to write\n",0
--- asm/messages_small.asm
+++ asm/messages_small.asm
@@ -1,11 +1,17 @@
+ALIGN 0x10
+
 welcome_msg:    
                 db "PainfulDiodes\n"
                 db "MARVIN Z80 monitor\n"
-                db "v1.1.0\n",0
+                db "v1.1.1_beta\n",0
 
+
+ALIGN 0x10
 
 bad_cmd_msg:    
                 db "Bad command\n",0
+
+ALIGN 0x10
 
 cmd_w_null_msg: 
                 db "No data\n",0
