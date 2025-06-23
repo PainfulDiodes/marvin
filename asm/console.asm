134b3713e4a0490360eeb9048490946bb233d47f
Stephen Willcock <stephen.willcock@gmail.com>
2025-06-23 08:00:22 +0100

    realign entry points
    Refactor console selection code so that the main code has consistent structure between build targets - less build-time logic, more run-time logic

--- asm/console.asm
+++ asm/console.asm
@@ -79,29 +79,32 @@ _puts_end:
     pop hl
     ret
 
-IF BEANBOARD
-; determine which console should be active - Reset=beanboard, shift-Reset=USB
+ALIGN 0x10
+
+; BeanZee console init - USB is the active console
 console_init:
+    ld a,CONSOLE_STATUS_USB
+    ld hl,CONSOLE_STATUS
+    ld (hl),a
+    ret
+
+ALIGN 0x10
+
+; determine which console should be active - Reset=beanboard, shift-Reset=USB
+beanboard_console_init:
     ; check for modifier keys being held down
     call modifierkeys
     ; shift key down?
     and MOD_KEY_SHIFT
-    jp nz,_console_init_usb
-;_console_init_beanboard:
+    ; yes shift
+    jp nz,_beanboard_console_init_usb
+    ; no shift
     ld a,CONSOLE_STATUS_BEANBOARD
     ld hl,CONSOLE_STATUS
     ld (hl),a
     ret
-_console_init_usb:
+_beanboard_console_init_usb:
     ld a,CONSOLE_STATUS_USB
     ld hl,CONSOLE_STATUS
     ld (hl),a
     ret
-ELSE
-; USB is the active console
-console_init:
-    ld a,CONSOLE_STATUS_USB
-    ld hl,CONSOLE_STATUS
-    ld (hl),a
-    ret
-ENDIF
