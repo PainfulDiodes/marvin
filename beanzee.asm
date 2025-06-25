eb004c6027fc7418d7e60a93040f4d0d14201e91
Stephen Willcock <stephen.willcock@gmail.com>
2025-06-25 07:15:41 +0100

    diverge build targets - console

--- beanzee.asm
+++ beanzee.asm
@@ -6,12 +6,9 @@ BEANBOARD EQU 0
 
 include "asm/main.inc"
 include "asm/escapestring.inc"
-include "asm/HD44780LCD.inc"
 include "asm/main.asm"
 include "asm/console.asm"
 include "asm/UM245R.asm"
 include "asm/marvin.asm"
 include "asm/strings.asm"
-include "asm/HD44780LCD.asm"
-include "asm/keymatrix.asm"
 include "asm/messages.asm"
