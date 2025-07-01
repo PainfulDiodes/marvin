c1875141c7ccc889ed3ca71d0f2345e62194600c
Stephen Willcock <stephen.willcock@gmail.com>
2025-07-01 07:47:55 +0100

    comment

--- asm/main.asm
+++ asm/main.asm
@@ -4,7 +4,7 @@ IF BEANBOARD
     call lcd_init
     call lcd_init_buffer
     call beanboard_console_init
-    call _beanboard_console_init_usb ; temporary lock to USB - TODO
+    call _beanboard_console_init_usb ; temporary lock to USB - TODO remove this line
 ENDIF
 ALIGN 0x0010 ; fix the warmstart address across targets
 WARMSTART:
