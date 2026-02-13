68eefe3e8caf2e1441684fbd3d24cfd5a68a1523
Stephen Willcock <160266765+PainfulDiodes@users.noreply.github.com>
2026-02-13 13:25:52 +0000

    update build: flatten output directory

--- targets/beanboard/build.sh
+++ targets/beanboard/build.sh
@@ -5,25 +5,24 @@
 # Default org: 0x0000
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-cd "$SCRIPT_DIR/../.."
+REPO_DIR="$SCRIPT_DIR/../.."
 
 org=${1:-0x0000}
-outdir=targets/beanboard/output
+outdir="$REPO_DIR/targets/beanboard/output"
 mkdir -p "$outdir"
 
-z88dk-z80asm -l -b -m -DMODULAR -DMARVINORG=$org \
-    asm/boot_beanboard.asm \
-    asm/console_beanboard.asm \
-    asm/beanboard_init.asm \
-    asm/UM245R.asm \
-    asm/marvin.asm \
-    asm/strings.asm \
-    asm/HD44780LCD.asm \
-    asm/keymatrix.asm \
-    asm/messages_small.asm \
+cd "$REPO_DIR/asm"
+z88dk-z80asm -l -b -m -I.. -DMODULAR -DMARVINORG=$org \
+    boot_beanboard.asm \
+    console_beanboard.asm \
+    beanboard_init.asm \
+    UM245R.asm \
+    marvin.asm \
+    strings.asm \
+    HD44780LCD.asm \
+    keymatrix.asm \
+    messages_small.asm \
     -O"$outdir"
 
-# z88dk-z80asm preserves source path structure under -O
-bindir="$outdir/asm"
-hexdump -C "$bindir/boot_beanboard.bin" > "$outdir/marvin.hex"
-z88dk-appmake +hex --org $org -b "$bindir/boot_beanboard.bin" -o "$outdir/marvin.ihx"
+hexdump -C "$outdir/boot_beanboard.bin" > "$outdir/marvin.hex"
+z88dk-appmake +hex --org $org -b "$outdir/boot_beanboard.bin" -o "$outdir/marvin.ihx"
--- targets/beanzee/build.sh
+++ targets/beanzee/build.sh
@@ -5,22 +5,21 @@
 # Default org: 0x0000
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-cd "$SCRIPT_DIR/../.."
+REPO_DIR="$SCRIPT_DIR/../.."
 
 org=${1:-0x0000}
-outdir=targets/beanzee/output
+outdir="$REPO_DIR/targets/beanzee/output"
 mkdir -p "$outdir"
 
-z88dk-z80asm -l -b -m -DMODULAR -DMARVINORG=$org \
-    asm/boot_beanzee.asm \
-    asm/console_usb.asm \
-    asm/UM245R.asm \
-    asm/marvin.asm \
-    asm/strings.asm \
-    asm/messages.asm \
+cd "$REPO_DIR/asm"
+z88dk-z80asm -l -b -m -I.. -DMODULAR -DMARVINORG=$org \
+    boot_beanzee.asm \
+    console_usb.asm \
+    UM245R.asm \
+    marvin.asm \
+    strings.asm \
+    messages.asm \
     -O"$outdir"
 
-# z88dk-z80asm preserves source path structure under -O
-bindir="$outdir/asm"
-hexdump -C "$bindir/boot_beanzee.bin" > "$outdir/marvin.hex"
-z88dk-appmake +hex --org $org -b "$bindir/boot_beanzee.bin" -o "$outdir/marvin.ihx"
+hexdump -C "$outdir/boot_beanzee.bin" > "$outdir/marvin.hex"
+z88dk-appmake +hex --org $org -b "$outdir/boot_beanzee.bin" -o "$outdir/marvin.ihx"
