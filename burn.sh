bed6351a47c1ac48731356fea916df763d9375be
Stephen Willcock <160266765+PainfulDiodes@users.noreply.github.com>
2026-02-20 18:32:05 +0000

    consolidate burn script

--- burn.sh
+++ burn.sh
@@ -1,6 +1,23 @@
-# usage: ./burn.sh target
+# usage: ./burn32k.sh [-m] [-8] target
+# -m : burn minimal build (marvin monitor only, no BBC BASIC)
+# -8 : use 8k EEPROM (AT28C64B) instead of 32k (AT28C256)
 
 set -x #echo on
 
+minimal=false
+device=AT28C256
+while getopts "m8" opt; do
+    case $opt in
+        m) minimal=true ;;
+        8) device=AT28C64B ;;
+    esac
+done
+shift $((OPTIND - 1))
+
 f=${1%.*} #extract base filename
-minipro -u -s -p AT28C64B -w targets/$f/output/marvin.bin
+
+if [ "$minimal" = true ]; then
+    minipro -u -s -p $device -w targets/$f/output/marvin_minimal.bin
+else
+    minipro -u -s -p $device -w targets/$f/output/marvin.bin
+fi
