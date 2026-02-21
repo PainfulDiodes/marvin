58fafe79b7e577db8345a5fe4a4ecdd4303077bf
Stephen Willcock <160266765+PainfulDiodes@users.noreply.github.com>
2026-02-21 06:20:31 +0000

    remove blank line between welcome message and prompt

--- asm/messages.asm
+++ asm/messages.asm
@@ -7,7 +7,7 @@ WELCOME_MSG:
                 db "MARVIN v1.2.1\n"
                 db "A simple Z80 homebrew monitor program\n"
                 db "(c) Stephen Willcock 2024\n"
-                db "https://github.com/PainfulDiodes\n\n",0
+                db "https://github.com/PainfulDiodes\n",0
 
 BAD_CMD_MSG:    
                 db "Command not recognised\n",0
