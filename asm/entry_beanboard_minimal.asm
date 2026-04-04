25a703dbed81f40ed25109baca91f800709747bb
Stephen Willcock <160266765+PainfulDiodes@users.noreply.github.com>
2026-04-04 11:44:34 +0100

    ra8875_console_set_background_colour

--- asm/entry_beanboard_minimal.asm
+++ asm/entry_beanboard_minimal.asm
@@ -30,6 +30,7 @@
     EXTERN ra8875_console_cursor_x ; console.asm - set cursor column
     EXTERN ra8875_console_cursor_y ; console.asm - set cursor row
     EXTERN ra8875_console_set_cursor_colour ; console.asm - set cursor colour
+    EXTERN ra8875_console_set_background_colour ; console.asm - set background colour
 
     PUBLIC START
 
@@ -77,6 +78,7 @@ ALIGN 0x0040
     jp ra8875_console_cursor_y      ; 0x007C - set cursor row (A = row, logical)
     jp ra8875_console_set_cursor_colour ; 0x007F - set cursor colour (RA8875_COL_* in A)
     jp hex_byte_val                 ; 0x0082 - parse hex pair from (HL), advance HL
+    jp ra8875_console_set_background_colour ; 0x0085 - set console background colour (A = colour)
 ;
 ; START stub - no BBC BASIC in minimal build
 START:
--- asm/entry_beanzee_minimal.asm
+++ asm/entry_beanzee_minimal.asm
@@ -63,6 +63,7 @@ ALIGN 0x0040
     jp _stub             ; 0x007C - ra8875_console_cursor_y (not available on beanzee)
     jp _stub             ; 0x007F - ra8875_console_set_cursor_colour (not available on beanzee)
     jp hex_byte_val      ; 0x0082 - parse hex pair from (HL), advance HL
+    jp _stub             ; 0x0085 - ra8875_console_set_background_colour (not available on beanzee)
 _stub:
     ret
 ;
