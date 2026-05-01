8b654c34760e282b27d8ae82bb4757aa88477c8c
Stephen Willcock <160266765+PainfulDiodes@users.noreply.github.com>
2026-05-01 12:37:00 +0100

    drive select case insensitive / help command

--- asm/messages.asm
+++ asm/messages.asm
@@ -1,9 +1,11 @@
     PUBLIC WELCOME_MSG
     PUBLIC BAD_CMD_MSG
     PUBLIC CMD_W_NULL_MSG
+    PUBLIC HELP_MSG
+    PUBLIC BDFS_HELP_MSG
 
 WELCOME_MSG:
-                db "MARVIN 2026-05-01\n"
+                db "MARVIN 2026-05-01a\n"
                 db "A simple Z80 homebrew monitor program\n"
                 db "(c) Stephen Willcock 2024\n"
                 db "https://github.com/PainfulDiodes\n",0
@@ -13,3 +15,16 @@ BAD_CMD_MSG:
 
 CMD_W_NULL_MSG:
                 db "No data to write\n",0
+
+HELP_MSG:
+                db "r <addr>         read memory\n"
+                db "w <addr> <data>  write memory\n"
+                db "x [addr]         execute (default 8000)\n"
+                db ":                load Intel HEX\n"
+                db "b / B            BASIC cold / warm start\n"
+                db "?                this help\n",0
+
+BDFS_HELP_MSG:
+                db "@A-F             select drive\n"
+                db "d                directory\n"
+                db "f [name]         format drive\n",0
